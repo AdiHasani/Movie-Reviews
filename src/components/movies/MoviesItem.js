4282a75c978016c5a1a511a7cdd812555194d0a2
Adnan Hasani <adnanhasani@gmail.com>
2019-09-16 14:08:14 +0200

    Few other properties added to the Movies Item and link to go to the articles review extern page

--- src/components/movies/MoviesItem.js
+++ src/components/movies/MoviesItem.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
 const MoviesItem = props => {
-  const { display_title, summary_short, multimedia } = props.movie;
+  const {
+    display_title,
+    headline,
+    summary_short,
+    multimedia,
+    link
+  } = props.movie;
   console.log(props);
   return (
-    <div className="card text-center">
+    <div className="card">
       <img src={multimedia.src} alt="IMG!" className="card-img" />
-      <h2>{display_title}</h2>
+      <h2 className="movie-title text-center mb-1">{display_title}</h2>
+      <h3>{headline}</h3>
       <p>{summary_short}</p>
+      <a className="btn" href={link.url}>
+        {link.suggested_link_text}
+      </a>
     </div>
   );
 };
