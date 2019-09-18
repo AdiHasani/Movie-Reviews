24479168206990f660f66720476edd698419c699
Adnan Hasani <adnanhasani@gmail.com>
2019-09-19 01:30:06 +0200

    Created wrapper component to scroll to top

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './components/pages/About';
 import MovieReview from './components/movies/MovieReview';
 import Notfound from './components/pages/Notfound';
 import Snackbar from './components/layout/Snackbar';
+import ScrollToTop from './components/ScrollToTop';
 import NyTimesState from './context/nytimes/NyTimesState';
 import SnackbarState from './context/snackbar/SnackbarState';
 import './App.css';
@@ -15,18 +16,20 @@ const App = () => {
     <NyTimesState>
       <SnackbarState>
         <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/movie/:title" component={MovieReview} />
-                <Route component={Notfound} />
-              </Switch>
-              <Snackbar />
-            </div>
-          </Fragment>
+          <ScrollToTop>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/movie/:title" component={MovieReview} />
+                  <Route component={Notfound} />
+                </Switch>
+                <Snackbar />
+              </div>
+            </Fragment>
+          </ScrollToTop>
         </Router>
       </SnackbarState>
     </NyTimesState>
